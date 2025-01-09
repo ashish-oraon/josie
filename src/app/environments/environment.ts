ef9a924effc84103c62d956913cf1506d37c35ef
Ashish Oraon <ashish.oraon@ect-telecoms.de>
2025-01-09 15:41:42 +0100

    APi updated to include 2025 sheets

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,10 +1,15 @@
 export const environment = {
   production: false,
-  apiUrlOld: 'https://script.google.com/macros/s/AKfycbxo32nXQuzbx69daD8pNfP76bVzxP2Q8kkgS1LHQTLQ-OEtNLaWiFDdh1zwWOQZrssp/exec',
-  apiUrlTillMay: 'https://script.google.com/macros/s/AKfycbyyPlmdRIYLu09XQRQB89myW80nAU2Kev--7hdHop3k0nyljw_4T8VTGDTn1oQWuHFT/exec',
-  apiUrl: 'https://script.google.com/macros/s/AKfycbzEPygIhLjEXRrlWZI-KV48KlZrB5umrXOy8hJjo6ete_sUAvRBaR3IAivonoXdCwd4/exec',
-  previousMonthsToShow:4,
+  apiUrlOld:
+    'https://script.google.com/macros/s/AKfycbxo32nXQuzbx69daD8pNfP76bVzxP2Q8kkgS1LHQTLQ-OEtNLaWiFDdh1zwWOQZrssp/exec',
+  apiUrlTillMay:
+    'https://script.google.com/macros/s/AKfycbyyPlmdRIYLu09XQRQB89myW80nAU2Kev--7hdHop3k0nyljw_4T8VTGDTn1oQWuHFT/exec',
+  apiUrlapiUrlTillDec:
+    'https://script.google.com/macros/s/AKfycbzEPygIhLjEXRrlWZI-KV48KlZrB5umrXOy8hJjo6ete_sUAvRBaR3IAivonoXdCwd4/exec',
+  apiUrl:
+    'https://script.google.com/macros/s/AKfycbxdFM3F6AQ5NSJ_HIGVI0Evcj2u2voc2Wo06YlHKcIY3yAo8Kbfu2aBtASYhyLEqI5d/exec',
+  previousMonthsToShow: 4,
   apiVersion: 'v1',
-  currency:'EUR',
-  users:['ashish','aishani']
+  currency: 'EUR',
+  users: ['ashish', 'aishani'],
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://script.google.com/macros/s/AKfycbxo32nXQuzbx69daD8pNfP76bVzxP2Q8kkgS1LHQTLQ-OEtNLaWiFDdh1zwWOQZrssp/exec',
+  // apiUrlOld: 'https://script.google.com/macros/s/AKfycbxo32nXQuzbx69daD8pNfP76bVzxP2Q8kkgS1LHQTLQ-OEtNLaWiFDdh1zwWOQZrssp/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbw6t5z-rXMbwF4b7lxB0j-xBFvfxQaq-PQtIK-Nuva10xHiz9n2twOU0UlnedQQq6Dl/exec',
   previousMonthsToShow:4,
   apiVersion: 'v1',
   currency:'EUR',
